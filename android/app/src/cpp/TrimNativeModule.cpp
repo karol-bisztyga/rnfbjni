371ebdc4ca1139dd7b7af17baeaf8bdde2e2a03a
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-05-10 09:45:56 +0200

    compiles without usage

--- android/app/src/cpp/TrimNativeModule.cpp
+++ android/app/src/cpp/TrimNativeModule.cpp
@@ -1,11 +1,16 @@
 #include "TrimNativeModule.h"
 
+#include <folly/MPMCQueue.h>
+
 namespace my_namespace {
 
 jsi::String TrimNativeModule::nativeTrim(jsi::Runtime &rt, const jsi::String &text) {
-    std::string str = text.utf8(rt);
-    if (str.length() > 10) {
-        str = str.substr(0, 7) + "...";
+  folly::MPMCQueue<int> q;
+
+  std::string str = text.utf8(rt);
+  if (str.length() > 10)
+  {
+    str = str.substr(0, 7) + "...";
     }
     return jsi::String::createFromUtf8(rt, str);
 }
