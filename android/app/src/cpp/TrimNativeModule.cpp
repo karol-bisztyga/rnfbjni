ce152ce9f4f49e56840c48121e5efa6a8be69a59
karol-bisztyga <karol.bisztyga@swmansion.com>
2021-05-14 16:38:51 +0200

    working

--- android/app/src/cpp/TrimNativeModule.cpp
+++ android/app/src/cpp/TrimNativeModule.cpp
@@ -5,15 +5,16 @@
 namespace my_namespace {
 
 jsi::String TrimNativeModule::nativeTrim(jsi::Runtime &rt, const jsi::String &text) {
-  folly::MPMCQueue<int> q;
+  folly::MPMCQueue<int> q(10);
   q.write(5);
+  q.write(6);
+  q.write(8);
 
   std::string str = text.utf8(rt);
-  if (str.length() > 10)
-  {
-    str = str.substr(0, 7) + "...";
-    }
-    return jsi::String::createFromUtf8(rt, str);
+  if (str.length() > 10) {
+    str = str.substr(0, 7) + "...("+ std::to_string(q.size()) +")";
+  }
+  return jsi::String::createFromUtf8(rt, str);
 }
 
 } // namespace my_namespace
